04a30c0ee0437b53f7ddafe6c10a9e52f2095e66
iMGamal <136174050+iMGamal@users.noreply.github.com>
2025-01-27 01:13:55 +0200

    Now We are set to run the merge sort

--- my-app/src/SortingVisualizer/SortingVisualizer.jsx
+++ my-app/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './SortingVisualizer.css';
+import {getMergeSortAnimations} from '../SortingAlgorithm/SortingAlgorithm.js';
+
+const ANIMATION_SPEED_MS = 1;
+const NUMBER_OF_ARRAY_BARS = 310;
+const PRIMARY_COLOR = 'turquoise';
+const SECONDARY_COLOR = 'red';
 
 class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -22,6 +28,30 @@ class SortingVisualizer extends React.Component {
         this.setState({ array });
     }
 
+    mergeSort() {
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('array-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+    }
+
     render() {
         const { array } = this.state;
         return (
@@ -31,9 +61,12 @@ class SortingVisualizer extends React.Component {
                 className="array-bar"
                 key={idx}
                 style={{
-                  height: `${value}px`,
+                    backgroundColor: PRIMARY_COLOR,
+                    height: `${value}px`,
                 }}></div>
             ))}
+            <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
             </div>
         );
     }
